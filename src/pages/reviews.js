9b89edcf02f6e5df5989a4c27893b99125cd2694
mubarekadem <mubarekademy.com>
2024-10-16 10:19:06 +0300

    added about,contact pages

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -1,12 +1,12 @@
 // pages/reviews.js
 import React from "react";
-import Reviews from "../components/commonComponents/Reviews"; // Ensure the correct import path
+import Reviews from "@/components/commonComponents/Reviews"; // Ensure the correct import path
 import Navbar from "@/components/commonComponents/Navbar";
 
 const ReviewsPage = () => {
   return (
     <div>
-    <Navbar
+      <Navbar />
       <Reviews />
     </div>
   );
